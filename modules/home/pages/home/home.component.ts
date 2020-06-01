76688310c9c1f05c2a0db7f33d50985c41406a20
Rory Standley <rorystandley@gmail.com>
2020-06-01 21:29:32 +0100

    Mocking news articles

--- modules/home/pages/home/home.component.ts
+++ modules/home/pages/home/home.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { NewsService } from '../../../../core/services/news.service';
 
-@Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
-})
+@Component( {
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: [ './home.component.scss' ]
+} )
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+	data;
 
-  ngOnInit() {
-  }
+	constructor( private newsService: NewsService ) {
+	}
+
+	ngOnInit() {
+		this.newsService.get().subscribe( resp => {
+			console.log( resp );
+			this.data = resp;
+		} );
+	}
 
 }
