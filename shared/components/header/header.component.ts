79a02fd4e82c6978bfd71b04eb155a71ad56e5b9
Rory Standley <rorystandley@gmail.com>
2020-06-13 16:08:47 +0100

    masthead for social icons

--- shared/components/header/header.component.ts
+++ shared/components/header/header.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component( {
 	selector: 'app-header',
 	templateUrl: './header.component.html',
-	styleUrls: ['./header.component.scss']
+	styleUrls: [ './header.component.scss' ]
 } )
 export class HeaderComponent implements OnInit {
+
+	@Input() facebook;
+	@Input() twitter;
+	@Input() instagram;
 
 	constructor() {
 	}
