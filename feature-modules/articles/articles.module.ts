1791321a5165b721e06ac46c90b385d3862f0903
Rory Standley <rorystandley@gmail.com>
2020-07-04 14:48:42 +0100

    Adding component

--- feature-modules/articles/articles.module.ts
+++ feature-modules/articles/articles.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroFourComponent } from './components/hero-four/hero-four.component';
 import { TwoUpThreeDownComponent } from './components/two-up-three-down/two-up-three-down.component';
+import { HeroTwoComponent } from './components/hero-two/hero-two.component';
 
 @NgModule( {
 	declarations: [
 		HeroFourComponent,
-		TwoUpThreeDownComponent
+		TwoUpThreeDownComponent,
+		HeroTwoComponent
 	],
 	imports: [
 		CommonModule
---
+++ feature-modules/articles/components/hero-two/hero-two.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component( {
+	selector: 'app-hero-two',
+	templateUrl: './hero-two.component.html',
+	styleUrls: [ '../../article.module.scss' ]
+} )
+export class HeroTwoComponent implements OnInit {
+
+	constructor() {
+	}
+
+	ngOnInit() {
+	}
+
+}
